c7c0d27377c2f40031247de2267b9d89bf0d92b7
wenke <wenke@xylink.com>
2025-05-08 17:12:54 +0800

    modify code

--- whttp-server-core/WHttpServer.cpp
+++ whttp-server-core/WHttpServer.cpp
@@ -695,7 +695,7 @@ void WHttpServer::handleHttpMsg(shared_ptr<HttpReqMsg> &httpMsg, WHttpServerApiD
         if (methods.find(httpMsg->method) == methods.end())
         {
             WLogw("WHttpServer::handleHttpMsg wrong http method: %s, uri: %s", httpMsg->method.c_str(), httpMsg->uri.c_str());
-            httpReplyJson(httpMsg, 500, "", formJsonBody(HTTP_UNKNOWN_REQUEST, "do not support this method"));
+            httpReplyJson(httpMsg, 405, "", formJsonBody(HTTP_UNKNOWN_REQUEST, "do not support this method"));
             closeHttpConnection(httpMsg->httpConnection);
             return;
         }
@@ -723,7 +723,7 @@ void WHttpServer::handleChunkHttpMsg(shared_ptr<HttpReqMsg> &httpMsg, WHttpServe
     set<string> methods = getSupportMethods(chunkHttpCbData.httpMethods);
     if (methods.find(httpMsg->method) == methods.end())
     {
-        httpReplyJson(httpMsg, 500, "", formJsonBody(HTTP_UNKNOWN_REQUEST, "do not support this method"));
+        httpReplyJson(httpMsg, 405, "", formJsonBody(HTTP_UNKNOWN_REQUEST, "do not support this method"));
         closeHttpConnection(httpMsg->httpConnection);
         return;
     }
